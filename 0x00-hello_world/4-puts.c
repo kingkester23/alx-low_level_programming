233b6b57e96db89627a947c567aa5f1dae6e0240
kingkester23 <kimwaswa1@gmail.com>
2023-06-16 17:12:43 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints the phrase "A program that builds a multilingual line
- * Return: 0
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
  */
-int nain(void)
-[
-puts(puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
-]
+}
