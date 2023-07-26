f6f237bc92758d13c53ed15316544bc8da6b34f9
kingkester23 <kimwaswa1@gmail.com>
2023-07-26 16:01:32 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,20 +12,20 @@ char *_strchr(char *s, char c)
 
 {
 
-        int i = 0;
+int i = 0;
 
 
-        for (; s[i] >= '\0'; i++)
+for (; s[i] >= '\0'; i++)
 
-        {
+{
 
-                if (s[i] == c)
+if (s[i] == c)
 
-                        return (&s[i]);
+return (&s[i]);
 
-        }
+}
 
-        return (0);
+return (0);
 
 }
 
