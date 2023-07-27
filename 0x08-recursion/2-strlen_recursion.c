0e9004158f4b924677fa5bdf8330975c06693267
kingkester23 <kimwaswa1@gmail.com>
2023-07-27 08:35:38 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -11,16 +11,16 @@ int _strlen_recursion(char *s)
 
 {
 
-        int longit = 0;
-        if (*s)
-        {
+int longit = 0;
+if (*s)
+{
 
-                longit++;
-                longit += _strlen_recursion(s + 1);
+longit++;
+longit += _strlen_recursion(s + 1);
 
-        }
+}
 
-        return (longit);
+return (longit);
 
 }
 
